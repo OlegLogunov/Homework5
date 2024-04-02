c4e8b549a3280006f34e1807ffe27cd563ddbbab
OlegLogunov <164496716+OlegLogunov@users.noreply.github.com>
2024-04-02 14:01:45 +0300

    Add files via upload

---
+++ HomeWork5.py
@@ -0,0 +1,13 @@
+my_list = ["Italy", "Spain", "Russia", "Germany", "Austria"]
+print("List: ", my_list)
+print("Firt element:", my_list[0])
+print("Last element:", my_list[-1])
+print("Sublist:", my_list[2:5])
+my_list[2] = "Australia"
+print("Modified list:", my_list)
+
+my_dict = {"Sun":"Солнце", "Moon":"Луна", "Earth":"Земля"}
+print("Dictionary: ", my_dict)
+print("Translation: ", my_dict["Earth"])
+my_dict["Ocean"] = "Океан"
+print("Modified dictionary: ", my_dict)
